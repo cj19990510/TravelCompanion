ba701629b7acf13aa1cfc3419bd119cf522ef1a1
ITCJie <1875763391@qq.com>
2019-08-06 11:44:05 +0800

    更新

--- src/cn/com/zx/travelcompanion/daoimp/HotelAdminDaoImp.java
+++ src/cn/com/zx/travelcompanion/daoimp/HotelAdminDaoImp.java
@@ -1,5 +1,13 @@
 package cn.com.zx.travelcompanion.daoimp;
 
-public interface HotelAdminDaoImp {
+import cn.com.zx.travelcompanion.bean.HotelInfoBean;
 
+public interface HotelAdminDaoImp {
+	//获得景点信息
+	public HotelInfoBean getHotelInfo(String type);
+	//添加酒店信息
+	public int addHotelInfo(HotelInfoBean hotel);
+	//
+	public int updateHotelInfo(HotelInfoBean hotel);
+	public int deleteHotelInfo(HotelInfoBean hotel);
 }
--- src/cn/com/zx/travelcompanion/daoimp/HotelInfoDaoImp.java
+++ src/cn/com/zx/travelcompanion/daoimp/HotelInfoDaoImp.java
@@ -1,5 +1,16 @@
 package cn.com.zx.travelcompanion.daoimp;
 
+import java.util.List;
+
+import cn.com.zx.travelcompanion.bean.HotelInfoBean;
+
 public interface HotelInfoDaoImp {
+	     //获得景点信息
+		public List<HotelInfoBean> getHotelInfo(String type);
+		//添加酒店信息
+		public int addHotelInfo(HotelInfoBean hotel);
+		//
+		public int updateHotelInfo(HotelInfoBean hotel);
+		public int deleteHotelInfo(HotelInfoBean hotel);
 
 }
