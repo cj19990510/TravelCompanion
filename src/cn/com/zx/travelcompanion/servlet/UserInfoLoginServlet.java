d2cb336ba4a83b1ef57643d7213669fcf00ea26f
ITCJie <1875763391@qq.com>
2019-08-07 08:54:30 +0800

    8.54 第一次更新

--- src/cn/com/zx/travelcompanion/servlet/UserInfoLoginServlet.java
+++ src/cn/com/zx/travelcompanion/servlet/UserInfoLoginServlet.java
@@ -1,4 +1,8 @@
 package cn.com.zx.travelcompanion.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
 @WebServlet("/UserInfoLoginServlet")
 public class UserInfoLoginServlet extends HttpServlet {
 
